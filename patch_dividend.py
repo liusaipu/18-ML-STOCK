3e5eba651cef76bd97918440586c112a3efeef9f
Stock Analyzer Dev <dev@stockanalyzer.local>
2026-04-15 13:47:59 +0800

    chore: remove temporary patch scripts

--- patch_dividend.py
+++
@@ -1,9 +0,0 @@
-with open('downloader/eastmoney.go', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if 'quote.PB = parseAnyFloat(resp.Data["f23"])' in line:
-        lines.insert(i+1, '\t\t\t\tquote.DividendYield = parseAnyFloat(resp.Data["f133"]) / 100 // 接口返回百分比数值，转为小数\n')
-        break
-with open('downloader/eastmoney.go', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('done')
